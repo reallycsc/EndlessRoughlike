d33d16794c561bd572f893fe90bc777b79244abc
reallycsc <reallyc2@sina.com>
2016-08-23 16:06:48 +0800

    iOS build for multi-touches

--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -17,52 +17,59 @@ public class PlayerCtrl : MonoBehaviour
 	// Update is called once per frame
 	void Update () {
         //MouseCtrl();
-	    TouchCtrl();
+		if (Input.touchCount == 1)
+	    	TouchMoveCtrl ();
+		if (Input.touchCount > 1) {
+			TouchMoveCtrl ();
+			TouchShootCtrl ();
+		}
 	}
 
     void MouseCtrl()
     {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 touchPos0 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            float angle0 = -Mathf.Rad2Deg * Mathf.Atan((touchPos0.x - transform.position.x) / (touchPos0.y - transform.position.y));
+            if (touchPos0.y - transform.position.y < 0)
+                angle0 += 180;
+            _directionTransform.eulerAngles = new Vector3(0, 0, angle0);
+            transform.Translate((touchPos0 - transform.position) * MoveSpeed * Time.deltaTime);
+        }
         if (Input.GetMouseButton(1))
         {
-            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 touchPos1 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            float angle = -Mathf.Rad2Deg * Mathf.Atan((touchPos.x - transform.position.x) / (touchPos.y - transform.position.y));
-            if (touchPos.y - transform.position.y < 0)
-                angle += 180;
-            _directionTransform.eulerAngles = new Vector3(0, 0, angle);
-            transform.Translate((touchPos - transform.position) * MoveSpeed * Time.deltaTime);
-        }
-        else if (Input.GetMouseButton(0))
-        {
-            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            float angle = -Mathf.Rad2Deg * Mathf.Atan((touchPos.x - transform.position.x) / (touchPos.y - transform.position.y));
-            if (touchPos.y - transform.position.y < 0)
-                angle += 180;
-            _gunBaseTransform.eulerAngles = new Vector3(0, 0, angle);
+            float angle1 = -Mathf.Rad2Deg * Mathf.Atan((touchPos1.x - transform.position.x) / (touchPos1.y - transform.position.y));
+            if (touchPos1.y - transform.position.y < 0)
+                angle1 += 180;
+            _gunBaseTransform.eulerAngles = new Vector3(0, 0, angle1);
         }
     }
 
-    void TouchCtrl()
-    {
-        if (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    void TouchMoveCtrl()
+	{
+		if (Input.GetTouch (0).phase == TouchPhase.Moved || Input.GetTouch (0).phase == TouchPhase.Began) {
+			Vector3 touchPos = Camera.main.ScreenToWorldPoint (Input.GetTouch(0).position);
 
-            float angle = -Mathf.Rad2Deg * Mathf.Atan((touchPos.x - transform.position.x) / (touchPos.y - transform.position.y));
-            if (touchPos.y - transform.position.y < 0)
-                angle += 180;
-            _directionTransform.eulerAngles = new Vector3(0, 0, angle);
-            transform.Translate((touchPos - transform.position) * MoveSpeed * Time.deltaTime);
-        }
-        if (Input.GetTouch(1).phase == TouchPhase.Moved || Input.GetTouch(1).phase == TouchPhase.Began)
-        {
-            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			float angle = -Mathf.Rad2Deg * Mathf.Atan ((touchPos.x - transform.position.x) / (touchPos.y - transform.position.y));
+			if (touchPos.y - transform.position.y < 0)
+				angle += 180;
+			_directionTransform.eulerAngles = new Vector3 (0, 0, angle);
+			transform.Translate ((touchPos - transform.position) * MoveSpeed * Time.deltaTime);
+		}
+	}
 
-            float angle = -Mathf.Rad2Deg * Mathf.Atan((touchPos.x - transform.position.x) / (touchPos.y - transform.position.y));
-            if (touchPos.y - transform.position.y < 0)
-                angle += 180;
-            _gunBaseTransform.eulerAngles = new Vector3(0, 0, angle);
-        }
+	void TouchShootCtrl()
+	{
+		if (Input.GetTouch (1).phase == TouchPhase.Moved || Input.GetTouch (1).phase == TouchPhase.Began) {
+			Vector3 touchPos = Camera.main.ScreenToWorldPoint (Input.GetTouch(1).position);
+
+			float angle = -Mathf.Rad2Deg * Mathf.Atan ((touchPos.x - transform.position.x) / (touchPos.y - transform.position.y));
+			if (touchPos.y - transform.position.y < 0)
+				angle += 180;
+			_gunBaseTransform.eulerAngles = new Vector3 (0, 0, angle);
+		}
     }
 }
